d1bbcdb4563c3227da38457c2c223ed6ed01506f
cgdm35 <107307047+cgdm35@users.noreply.github.com>
2022-09-20 09:39:51 +0300

    Update odev-10.sql

--- odev-10.sql
+++ odev-10.sql
@@ -3,18 +3,18 @@
 --1) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
 
 SELECT city.city, country.country
-FROM country
-LEFT JOIN city
-ON country.country_id=city.country_id;
+FROM city
+LEFT JOIN country
+ON city.country_id=country.country_id;
 
 
 --2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte
 görebileceğimiz RIGHT JOIN sorgusunu yazınız.
 
 SELECT payment.payment_id, customer.first_name, customer.last_name
-FROM payment
-RIGHT JOIN customer
-ON payment.customer_id=customer.customer_id;
+FROM customer
+RIGHT JOIN payment
+ON customer.customer_id=payment.customer_id;
 
 
 
@@ -22,7 +22,7 @@ ON payment.customer_id=customer.customer_id;
 isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 
 SELECT rental.rental_id, customer.first_name, customer.last_name
-FROM rental
-FULL JOIN customer
-ON rental.customer_id=customer.customer_id;
+FROM customer
+FULL JOIN rental
+ON customer.customer_id=rental.customer_id;
 
